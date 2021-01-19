52bc78444d0a32d4ad15708cfd7f61cc25115a2e
nreckle <nreckle@fake.com>
2021-01-19 09:00:02 +0800

    Upgrade buildTool and migirate to Androidx

--- examples/PoC/app/src/main/java/com/sletmo/launcherclientpoc/SimpleFragment.java
+++ examples/PoC/app/src/main/java/com/sletmo/launcherclientpoc/SimpleFragment.java
@@ -1,8 +1,8 @@
 package com.sletmo.launcherclientpoc;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +11,6 @@ public class SimpleFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment, container, false);
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment, container, false);
     }
 }
